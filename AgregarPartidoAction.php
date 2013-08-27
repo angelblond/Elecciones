969c3bc42e0a7562d8b615d65c7874ad9aa5dce9
Angel David Rubio <ceangelblond@gmail.com>
2013-08-27 03:20:45 -0430

    Pequenas variaciones. Segunda version.

--- AgregarPartidoAction.php
+++ AgregarPartidoAction.php
@@ -3,5 +3,7 @@
 	$nombre = $_POST['partido'];
 	$tendencia = $_POST['tendencia'];
 	$conexion = conectorBD::getInstance();
+	$nuevoPartido = new Partido($nombre, $tendencia);
+	$nuevoPartido.agregarPartido();
 	
 ?>

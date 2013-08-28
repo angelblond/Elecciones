de9ae203e347720e911a778fa645b2d064c5915c
angelblond <david_angel007@hotmail.com>
2013-08-27 21:06:39 -0430

    Update AgregarPartidoAction.php
    Mejorado via web el agregar partido

--- AgregarPartidoAction.php
+++ AgregarPartidoAction.php
@@ -6,6 +6,9 @@
 	$nuevoPartido = new Partido($nombre, $tendencia);
 	$inserta = $nuevoPartido.agregarPartido();
 	if (inserta == 0){
-		header("Location: Mensaje.php?comando=AgregarExito");
+		header("Location: Mensaje.php?comando=AgregarPExito");
+	}
+	else{
+		header("Location: Mensaje.php?comando=AgregarPFracaso");
 	}
 ?>
